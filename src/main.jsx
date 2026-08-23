d3d5ba98ed8952b8352c57e9e7a20569822b2171
rafikmoha <m63866157@gmail.com>
2026-08-23 18:37:24 +0100

    feat: add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
-import ReactDOM from 'react-dom/client';
+import react from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App.jsx';
-import './index.css';
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import { injectAnalytics } from "@vercel/analytics";
+
+injectAnalytics();
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
